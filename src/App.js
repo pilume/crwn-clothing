7047707eb3da58270fe673429f34779bb2237ed4
pilume <you@example.com>
2020-11-21 17:41:38 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Homepage from './homepage.componet'
+
+function App() {
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
